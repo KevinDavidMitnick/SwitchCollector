96072c1b53e5d687aba7f217e825a1eba997dccf
liucong <liucong@chinac.com>
2018-06-26 10:01:33 +0800

    add udp server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
+	"net"
 	"net/http"
 	"time"
 )
@@ -47,10 +48,40 @@ func writeMessage(conn *websocket.Conn, mt int, msg []byte) {
 	}
 }
 
+func startUdpServ() {
+	// create upd socket
+	log.Println("starting udp server....")
+	socket, err := net.ListenUDP("udp4", &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 514,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer socket.Close()
+
+	for {
+		// read data.
+		data := make([]byte, 1024)
+		size, remoteAddr, err := socket.ReadFromUDP(data)
+		if err != nil {
+			log.Println("read data from udp failed,remote ip :" + string(remoteAddr.IP))
+			continue
+		}
+		go handleUdpData(data, size)
+	}
+}
+
+func handleUdpData(data []byte, size int) {
+	fmt.Println(data)
+}
+
 func main() {
 	fmt.Println("starting server....")
 	http.HandleFunc("/echo", echo)
 	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+	go http.ListenAndServe("0.0.0.0:8080", nil)
+
+	go startUdpServ()
 	select {}
 }

aae50f081438785ba644f1f7aa17fb7960eb8aa8
liucong <liucong@chinac.com>
2018-07-13 16:27:33 +0800

    finish swping

--- service/funcs/swping.go
+++ service/funcs/swping.go
@@ -13,7 +13,7 @@ import (
 
 //Ping check,switch ping .
 func Ping(ip string, timeout int) (int64, int64) {
-	cmd := exec.Command("ping " + ip + " -c 2")
+	cmd := exec.Command("/bin/bash", "-c", "ping "+ip+" -c 2")
 	var stdout bytes.Buffer
 	cmd.Stdout = &stdout
 	var stderr bytes.Buffer
@@ -38,9 +38,11 @@ func Ping(ip string, timeout int) (int64, int64) {
 	rrts := strings.Fields(string(data))
 	for _, str := range rrts {
 		if strings.Contains(str, "time") {
-			temp := strings.Split(str, "=")[1]
-			if value, err := strconv.ParseFloat(temp, 64); err == nil {
-				rrt += value
+			temp := strings.Split(str, "=")
+			if len(temp) == 2 {
+				if value, err := strconv.ParseFloat(strings.TrimSpace(temp[1]), 64); err == nil {
+					rrt += value
+				}
 			}
 		}
 	}

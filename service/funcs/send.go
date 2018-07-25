37d81719623b14d1aeb174dc79efb9bacb04b8a1
liucong <liucong@chinac.com>
2018-07-25 17:57:25 +0800

    add data print

--- service/funcs/send.go
+++ service/funcs/send.go
@@ -13,6 +13,7 @@ func Send(addr string, data interface{}) {
 		fmt.Println("send err , data is:", data)
 		return
 	}
+	fmt.Println("send :%s,data :%s", addr, string(buf))
 	request, _ := http.NewRequest("POST", addr, bytes.NewBuffer(buf))
 	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
 	request.Header.Set("TIMEOUT", "10")
